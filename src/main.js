ea01de8fa194b9ed51d2cbf2997ad6ad746d6b91
Irina Varushkina <rnvr@mail.ru>
2026-06-15 17:33:06 +0500

    Sprint 7: временно отключает  использование поиска, фильтрации, сортировки, пагинации

--- src/main.js
+++ src/main.js
@@ -40,10 +40,10 @@ function render(action) {
     let state = collectState(); // состояние полей из таблицы
     let result = [...data]; // копируем для последующего изменения
     // @todo: использование
-    result = applySearch(result, state, action);
-    result = applyFiltering(result, state, action);
-    result = applySorting(result, state, action);
-    result = applyPagination(result, state, action);
+    // result = applySearch(result, state, action);
+    // result = applyFiltering(result, state, action);
+    // result = applySorting(result, state, action);
+    // result = applyPagination(result, state, action);
 
     sampleTable.render(result)
 }
@@ -73,9 +73,9 @@ const applySorting = initSorting([
     sampleTable.header.elements.sortByTotal
 ]);
 
-const applyFiltering = initFiltering(sampleTable.filter.elements, {
-    searchBySeller: indexes.sellers
-});
+// const applyFiltering = initFiltering(sampleTable.filter.elements, {
+//     searchBySeller: indexes.sellers
+// });
 
 const applySearch = initSearching('search');
 
